942bab784c506d240009f3782d5d8b21533d6bb2
Mark King <markpkng@gmail.com>
2020-02-24 21:42:21 -0500

    change foreign key relationships to use hyperlinks

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,12 +1,43 @@
-from django.urls import path, include
+from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
-urlpatterns = [
-    path('users', views.UserList.as_view()),
-    path('snippets', views.SnippetList.as_view()),
-    path('snippets/<int:pk>', views.SnippetDetail.as_view()),
-    path('api-auth', include('rest_framework.urls'))
-]
+# API endpoints
+urlpatterns = format_suffix_patterns([
+    path('', views.api_root),
+    path('snippets/',
+         views.SnippetList.as_view(),
+         name='snippet-list'),
+    path('snippets/<int:pk>/',
+         views.SnippetDetail.as_view(),
+         name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/',
+         views.SnippetHighlight.as_view(),
+         name='snippet-highlight'),
+    path('users/',
+         views.UserList.as_view(),
+         name='user-list'),
+    path('users/<int:pk>/',
+         views.UserDetail.as_view(),
+         name='user-detail')
+])
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+# --------------------------------------------------------------------------
+# before hyperlinking
+
+# from django.urls import path, include
+# from rest_framework.urlpatterns import format_suffix_patterns
+# from snippets import views
+
+# urlpatterns = [
+#     path('', views.api_root)
+#     path('users', views.UserList.as_view()),
+#     path('snippets', views.SnippetList.as_view()),
+#     path('snippets/<int:pk>', views.SnippetDetail.as_view()),
+#     path('api-auth', include('rest_framework.urls')),
+#     path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()),
+# ]
+
+# urlpatterns = format_suffix_patterns(urlpatterns)
+
+# # --------------------------------------------------------------------------
