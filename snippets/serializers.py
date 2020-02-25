eec90decb1cb654d20c8e0f7ff5c3d144b9831ef
Mark King <markpkng@gmail.com>
2020-02-24 20:41:34 -0500

    add owner, user endpoints, associate snippets with users, update serialzier for creating snippets

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,12 +1,23 @@
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = Userfields = ['id', 'username', 'snippets']
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = Snippet
         fields = ['id', 'title',
-                  'code', 'linenos', 'language', 'style']
+                  'code', 'linenos', 'language', 'style', 'owner']
 
 # ModelSerializer is just a shortcut for creating serializer classes, and provides
 # simple default implementations for the create() and update() methods
