66d5516b7881b5788d1d6be2c51f4cc8ebe325e1
Mark King <markpkng@gmail.com>
2020-02-24 20:57:38 -0500

    add login to browsable api

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
@@ -6,6 +6,7 @@ urlpatterns = [
     path('users', views.UserList.as_view()),
     path('snippets', views.SnippetList.as_view()),
     path('snippets/<int:pk>', views.SnippetDetail.as_view()),
+    path('api-auth', include('rest_framework.urls'))
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
